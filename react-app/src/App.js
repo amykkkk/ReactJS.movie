a1ae552b328588155d921ec3f7a48ad6c11b3f49
kimayeong <kimayeong@valofe.com>
2023-03-15 15:55:01 +0900

    #7.2 Coin Tracker

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -1,53 +1,69 @@
 import { useState, useEffect } from "react";
+import styled from "./style.css";
 
 function App() {
-  const [toDo, setTodo] = useState("");
-  const [toDos, setToDos] = useState([]);  // empty array
-  const onchange = (event) => setTodo(event.target.value);
-  const onSubmit = (event) => {   // submit(기본 form > button) 이벤트
-    event.preventDefault();       // form 기본 이벤트 막기(submit)
-    if (toDo === "") {
-      return;                     // toDo가 비어있으면 함수가 작동하지않도록 return
-    }
-    setToDos((currentArray) => [...currentArray, toDo]); // array 추가 > ... + array
-    setTodo(""); // submit > empty
-  };
-  const deleteBtn = index => {
-    /* 다른 방법들 
-      const li = event.target.parentElement;
-      li.remove();
-      setTodos(todos.filter((item, todoIndex) => index !== todoIndex));
-    */
+  const [loading, setLoading] = useState(true);      // data 가져오기전 loading
+  const [coins, setCoins] = useState([]);
+  const [price, setPrice] = useState(0);             // coin 값
+  const [amount, setAmount] = useState("");          // input 값
 
-    setToDos((curToDos) =>
-      // 선택한 값의 index와 index가 다른 값들만 filter,'_': 사용하지 않는 변수를 표기(item 첫번째 인수 생략 불가)
-      curToDos.filter((_, curIndex) => curIndex !== index)
-    );
-  };
-  console.log(toDos)
+  useEffect(() => {
+    fetch("https://api.coinpaprika.com/v1/tickers")  // 정보 가져올 url
+      .then((response) => response.json())           // url에서 가져온 정보를 json으로 변환
+      .then((json) => setCoins(json));               // 변환된 json을 coins(빈 array)에 넣기
+    setLoading(false);                               // loading 글자 지우기
+  }, []);
+  
+  const handleInput = (e) => setAmount(e.target.value);
+  const handlePrice = (event) => {setPrice(event.target.value);};
 
   return (
-    <div>
-      <h1>My todos ({toDos.length})</h1> {/* js는 {}안에 사용 */}
-      <form onSubmit={onSubmit}>
-        <input
-          onChange={onchange}
-          value={toDo}
-          type="text"
-          placeholder="Write your to do"
-        />
-        <button>Add To Do</button>
-      </form>
-      <hr />
-      {/* map : array 수정 */}
-      <ul>
-        {toDos.map((item, index) => (
-          <li key={index}>
-            {/* () => deleteBtn(index): index 값을 넘기기 위한 것 */}
-            {item} <button onClick={() => deleteBtn(index)}>❌</button>
-          </li>
-        ))}
-      </ul>
+    <div className="set">
+      <div className="title-container">
+        <h1>The coins! {loading ? "" : `(${coins.length})`}</h1>
+        <p>
+          Database:
+          <a href="https://api.coinpaprika.com/v1/tickers">Coin Paprika</a>
+        </p>
+      </div>
+
+      <div className="main-container">
+        <div>
+          <label className="label1">Dollars you have $ </label>
+          <input
+            onChange={handleInput}
+            value={amount}
+            type="number"
+            placeholder="Please write your USD"
+          />
+          <span> USD</span>
+        </div>
+        <br />
+
+        {loading ? (
+          <strong>Loading...</strong>
+        ) : (
+          <div>
+            <label className="label1">Choose coin :</label>
+            <select onChange={handlePrice}>
+              <option key="-1">Select Coin !</option>
+              {coins.map((coin) => (
+                <option key={coin.id} value={coin.quotes.USD.price.toFixed(2)}>
+                  {/* index 대신 coin.id 사용 */}
+                  {coin.name} ({coin.symbol})
+                </option>
+              ))}
+            </select>
+          </div>
+        )}
+
+        <div className="result">
+          <strong>
+            You can get &nbsp;
+            {price === 0 ? "0" : (amount / price).toFixed(2)} Coin(s).
+          </strong>
+        </div>
+      </div>
     </div>
   );
 }
