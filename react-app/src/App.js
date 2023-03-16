dcdd895b84661007088ea928e36591259e84f925
kimayeong <kimayeong@valofe.com>
2023-03-16 12:40:09 +0900

    경로수정

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -7,9 +7,9 @@ function App() {
   return (
     <BrowserRouter basename={process.env.PUBLIC_URL}>
       <Routes>
-        <Route path={`${process.env.PUBLIC_URL}/`} element={<Home />} />
+        <Route path={process.env.PUBLIC_URL + "/"} element={<Home />} />
         <Route
-          path={`${process.env.PUBLIC_URL}/movie/:id`}
+          path={process.env.PUBLIC_URL + "/movie/:id"}
           element={<Detail />}
         />
       </Routes>
